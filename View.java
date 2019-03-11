4804dddb034b4142597d0ca8cc9be1b02905d2ef
Vimple-Singh <33506691+Vimple-Singh@users.noreply.github.com>
2019-03-12 09:01:31 +1100

    Create View.java

---
+++ View.java
@@ -0,0 +1,11 @@
+public class View {
+
+	public void startProgram(String input) {
+		System.out.print("Press Any Key To Start The Program: ");
+	}
+
+	public void takeInput(String input) {
+		System.out.println("USER: ");
+
+	}
+}
